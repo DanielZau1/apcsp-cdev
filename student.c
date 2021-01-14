b43081398f46dcbe72f77a967aee7eb362297e84
Daniel Zau <danielzau1@yahoo.com>
2021-01-14 11:22:56 +0000

    edited student.c

--- student.c
+++ student.c
@@ -5,28 +5,28 @@
 // student structure
 //struct Student...
 struct Student{
-	char firstName[50];
-	char lastName[50];
-	int age;
-	int studentId;
-}
-void printStudent(struct Student* student)
+        char firstName[50];
+        char lastName[50];
+        int age;
+        int studentID;
+};
+
+void printStudent(struct Student* students)
 {
-  printf("First name:%s\n", student->firstName);
-  printf("Last name:%s\n", student->lastName);
-  printf("Enter age: %d\n", student->age);
-  printf("Enter id: %d\n", student->studentId);
+printf("Student:\n");
+printf("  First Name: %s\n", students->firstName);
+printf("  Last Name: %s\n", students->lastName);
+printf("  Age: %d\n", students->age);
+printf("  ID: %d\n", students->studentID);
 }
 
 
 void printAllStudents(struct Student students[], int num)
 {
   // call printStudent for each student in students
-  printf("---- student ----\n", );
-  printf("Student: %s %s\n",student->firstName, student->lastName);
-  printf("age: %d\n", student->age");
-  printf("id: %d\n", student->studentId");
 
+
+printStudent(students);
 }
 
 
@@ -35,18 +35,22 @@ int main()
 
   // an array of students
   //xxx students;
-  char students[20][50];
-  int numStudents = 0;
+
+int numStudents = 0;
+  struct Student students[numStudents];
+
+char input[256];
   while (1)
   {
     char c;
     printf("\nEnter a to add, p to print, q to quit:");
     fgets(input, 256, stdin);
     if (sscanf(input, "%c", &c) != 1) continue;
-    if (c == 'q') 
+    if (c == 'q')
     {
       break;
-    }
+
+}
     else if (c == 'p')
     {
       // print all students
@@ -56,11 +60,36 @@ int main()
     else if (c == 'a')
     {
       // enter a new student
-    struct student
 
       numStudents++;
+struct Student students[numStudents];
+
+char inputA[256];
+printf("First name:\n");
+fgets(inputA, 256, stdin);
+sscanf(inputA, "%s", students[numStudents].firstName);
+
+printf("Last name:\n");
+fgets(inputA, 256, stdin);
+sscanf(inputA, "%s", students[numStudents].lastName);
+
+printf("Age:\n");
+while(1)
+{
+fgets(inputA, 256, stdin);
+
+if (sscanf(input, "%d", &students[numStudents].age) == 1) break;
+printf("Not a valid age\n");
+}
+
+printf("Student ID:\n");
+while(1)
+{
+fgets(inputA, 256, stdin);
+if (sscanf(input, "%d", &students[numStudents].studentID) == 1) break;
+printf("Not a valid ID\n");
+}
     }
   }
-  
-  printf("\nGoodbye!\n");
+printf("\nGoodbye!\n");
 }
